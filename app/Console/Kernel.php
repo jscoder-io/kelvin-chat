9113cb8b23fddc9aa116666f387ad6a19c8d2cd9
Firman <firman.upwork@gmail.com>
2024-01-22 08:25:22 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        //$schedule->command('message:inbox')->everyMinute();
+        $schedule->command('message:inbox')->everyMinute();
         //$schedule->command('message:chat')->twiceDaily(1, 13);
 
         // $schedule->command('inspire')->hourly();
